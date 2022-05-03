0570596fb40dd50f861c08bbf3de7394af5a98a9
Mahendra Nath Reddy E <mahendranathreddy.e@gmail.com>
2022-05-03 20:13:52 +0530

    Added Python Files

---
+++ Loops.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 13 10:12:18 2022
+
+@author: Mahendra Nath Reddy E
+"""
+
+a=int(input())
+
+for i in range(0,a):
+    print(i*i)
+
+'''
+this is to get modified change for branch'''
